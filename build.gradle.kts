11e86429dfbff635470c6f33fb7642b12d1a5387
feliciaannabelruri <163542970+feliciaannabelruri@users.noreply.github.com>
2025-10-13 23:54:23 +0700

    Commit gradle change

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,12 @@
 @file:Suppress("DEPRECATION")
 
 plugins {
-    id("com.android.application") version "8.13.0" apply false
-    id("com.android.library") version "8.13.0" apply false
+    id("com.android.application") version "8.12.3" apply false
+    id("com.android.library") version "8.12.3" apply false
     kotlin("android") version "2.0.21" apply false
     id("com.google.gms.google-services") version "4.4.3" apply false
 }
 
-
-tasks.register("clean", Delete::class) {
+tasks.register<Delete>("clean") {
     delete(rootProject.buildDir)
 }
